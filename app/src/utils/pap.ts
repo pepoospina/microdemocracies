a66cd5efda3f4759d7f781baf11f0c4e28869cc3
Pepo <pepo.ospina@gmail.com>
2024-05-17 15:19:57 +0200

    show platform usernames

--- app/src/utils/pap.ts
+++ app/src/utils/pap.ts
@@ -2,14 +2,22 @@ import { PAP } from '../shared/types'
 
 export const getPapShortname = (pap?: PAP) => {
   if (!pap) return ''
-  if (pap.person && pap.person.personal) {
+  if (pap.person && pap.person.personal && Object.keys(pap.person.personal).length > 0) {
     return `${pap.person.personal.firstName ? pap.person.personal.firstName : ''}${
       pap.person.personal.lastName ? ` ${pap.person.personal.lastName}` : ''
     }`
   }
 
   if (pap.person.platforms) {
-    if (pap.person.platforms[0]) {
-    }
+    const activePlatforms = pap.person.platforms.filter((p) => p.username !== undefined)
+
+    if (activePlatforms.length === 1) return activePlatforms[0].username
+
+    const activePlatformsString = activePlatforms.reduce((acc, platform) => {
+      const thisPlatform = `${platform.platform}: ${platform.username}`
+      return acc ? `${acc}, ${thisPlatform}` : thisPlatform
+    }, '')
+
+    return activePlatformsString
   }
 }
